7979d6dd65a82c12122a45af2de59fe1227a5ed7
bhuvan <69458259+bhuvanbhoge@users.noreply.github.com>
2024-10-26 08:43:09 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,21 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
 export default authMiddleware({
-  // Allow signed out users to access the specified routes:
-  publicRoutes: ['/', '/api/webhooks/clerk', '/api/webhooks/stripe']
+  // Allow signed-out users to access the specified routes:
+  publicRoutes: [
+    "/",
+    "/api/webhooks/clerk",
+    "/api/webhooks/stripe",
+    "/transformations/add/remove",
+    "/transformations/add/fill",
+    "/transformations/add/store",
+    "/transformations/add/recolor",
+    "/transformations/add/removeBackground",
+  ],
 });
- 
+
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"
-  ]
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/(api|trpc)(.*)"],
 };
